a0c22d8cb10826c895b9276c4057cfead50fda55
Zhanarbek Osmonaliev <zhanarbek.osmonaliev@stonybrook.edu>
2021-03-04 21:58:44 +0900

    Material UI configured, Feather Icons added

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-sass`,
-    `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { Link } from "gatsby";
-
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  overrides: {
+    // To edit globals of Material Design
+  },
+});
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-  </Layout>
+  <ThemeProvider theme={theme}>
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+    </Layout>
+  </ThemeProvider>
 );
 
 export default IndexPage;
