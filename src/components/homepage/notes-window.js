3a22e3d994edfb9c56015cc081a37f19de40bf30
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2021-05-01 23:18:17 +0900

    Refactor Notes / Todolist window

--- src/components/homepage/notes-window.js
+++ src/components/homepage/notes-window.js
@@ -12,6 +12,7 @@ import {
   DialogContent,
   DialogActions,
   DialogContentText,
+  ClickAwayListener,
 } from "@material-ui/core";
 import { SvgIcon, IconButton, Button } from "@material-ui/core";
 import { X, XCircle, Plus } from "react-feather";
@@ -28,46 +29,21 @@ import {
 } from "@material-ui/core";
 import nextId from "react-id-generator";
 import produce from "immer";
-import { SortableContainer, SortableElement } from "react-sortable-hoc";
-import arrayMove from "array-move";
 
-const NotesWindow = forwardRef((props, ref) => {
+const NotesWindow = forwardRef(({ notes, setNotes }, ref) => {
   const [open, setOpen] = useState(false);
   const [scroll, setScroll] = useState("paper");
+  //index in the Notes list
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  //id of Notes in the list
+  const [selectedId, setSelectedId] = useState(
+    notes.length === 0 ? -1 : notes[0].id
+  );
   //local data for notes
-  const [notes, setNotes] = useState([
-    {
-      title: "CSE 416",
-      id: 0,
-      content:
-        "Introduces the basic concepts and modern tools and techniques of <br/> software engineering. Emphasizes the development of reliable and maintainable software via system requirements and specifications, software design methodologies including object-oriented design, implementation, integration, and testing; software project management; life-cycle documentation; software maintenance; and consideration of human factor issues.",
-      titleToggle: true,
-      contentToggle: true,
-    },
-    {
-      title: "Homeplus Grocery List",
-      id: 1,
-      content: "Apple, cereal, banana, ramen, tissues",
-      titleToggle: true,
-      contentToggle: true,
-    },
-    {
-      title: "SBU Visit Document",
-      id: 2,
-      content: "Action needs to be done. ",
-      titleToggle: true,
-      contentToggle: true,
-    },
-  ]);
-  //track whether a user makes change in notes title column
-  const [notesTitleFocus, setNotesTitleFocus] = useState(false);
-  //track whether a user makes change in notes content
-  const [notesContentFocus, setNotesContentFocus] = useState({
-    focus: false,
-    index: -1,
-  });
+  const myRef = useRef(null);
   //keep track of notes list index
-  const nextIndexNote = useRef(3);
+
+  const nextIndexNote = useRef(notes.length);
   //Context menu's initial position (= small popup after right-click)
   const initialMousPos = {
     mouseX: null,
@@ -78,6 +54,7 @@ const NotesWindow = forwardRef((props, ref) => {
   //Keep track which todolist user right-clicks
   const [notesIdForContextMenu, setNotesIdForContextMenu] = useState(null);
 
+  //Open / Close Modal
   const handleClickOpen = () => {
     setOpen(true);
     setScroll("paper");
@@ -85,6 +62,7 @@ const NotesWindow = forwardRef((props, ref) => {
   const handleClose = () => {
     setOpen(false);
   };
+
   //to handle context menu
   const handleContextMenu = (e, id) => {
     if (id != null) {
@@ -105,63 +83,31 @@ const NotesWindow = forwardRef((props, ref) => {
   const handleContextMenuClose = () => {
     setMousePos(initialMousPos);
   };
-  //index in the Notes list
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  //id of Notes in the list
-  const [selectedId, setSelectedId] = useState(0);
 
-  const [displayedNotes, setDisplayedNotes] = useState(
-    notes.filter((note) => note.id === selectedId)
-  );
+  const handleContextMenuDeleteNotes = useCallback(() => {
+    setMousePos(initialMousPos);
+    setNotes(notes.filter((note) => note.id !== notesIdForContextMenu));
+    if (notes.length != 1) {
+      //Reset to first todolist
+      if (notesIdForContextMenu == notes[0].id) {
+        setSelectedId(notes[1].id);
+      } else {
+        setSelectedId(notes[0].id);
+      }
+      setSelectedIndex(0);
+    } else {
+      setSelectedId(-1);
+    }
+    setNotesIdForContextMenu(null);
+    nextIndexNote.current -= 1;
+  });
 
-  //FIX HERE
+  //Handle displaying note
   const handleSelectList = (e, id) => {
     setSelectedId(id);
     setSelectedIndex(notes.findIndex((note) => note.id === id));
   };
 
-  //when "Add new note" is clicked
-  useEffect(() => {
-    if (!notesTitleFocus) {
-      setNotes(
-        produce(notes, (draft) => {
-          draft.map((note) =>
-            !note.contentToggle
-              ? (note.contentToggle = true)
-              : note.contentToggle
-          );
-        })
-      );
-      setNotes(
-        produce(notes, (draft) => {
-          draft.map((note) =>
-            note.title === "" ? (note.title = "New Note") : note.title
-          );
-        })
-      );
-    }
-    console.log("useEffect 1");
-  }, [notesContentFocus.focus]);
-
-  useEffect(() => {
-    if (selectedId != -1) {
-      setDisplayedNotes(notes.filter((note) => note.id === selectedId));
-    }
-    console.log("useEffect 2");
-  }, [selectedId, notes[selectedIndex]]);
-
-  useEffect(() => {
-    const newFocus = { focus: false, id: -1 };
-    setNotesContentFocus(newFocus);
-    console.log("useEffect 3");
-  }, [selectedIndex]);
-
-  // useEffect(() => {
-  //   if (notesContentFocus) {
-  //     setNotesTitleFocus(false);
-  //   }
-  //   console.log("useEffect 4");
-  // }, [notesContentFocus]);
   //user can double click the title and change it
   const handleDoubleClickTitle = () => {
     setNotes(
@@ -169,10 +115,11 @@ const NotesWindow = forwardRef((props, ref) => {
         draft[selectedIndex].titleToggle = false;
       })
     );
-    setNotesTitleFocus(true);
+
     console.log("handle double click title");
   };
 
+  //Handle Changes for note's title / content
   const handleTitleChange = (e) => {
     setNotes(
       produce(notes, (draft) => {
@@ -191,82 +138,8 @@ const NotesWindow = forwardRef((props, ref) => {
     console.log("handle change content");
   };
 
-  // Working on Todolist
-  const handleKeyDownNotesTitle = (e) => {
-    if (notes.length == 0) {
-      return;
-    }
-    if (e.type === "click" || e.key === "Enter") {
-      if (notesTitleFocus) {
-        setNotes(
-          produce(notes, (draft) => {
-            console.log(draft);
-            draft[selectedIndex].title =
-              draft[selectedIndex].title === ""
-                ? "New Note"
-                : draft[selectedIndex].title;
-            draft[selectedIndex].titleToggle = true;
-          })
-        );
-        setNotesTitleFocus(false);
-        setNotesContentFocus(true);
-      }
-    }
-    console.log("handleKeyDown Notes Title function");
-  };
-
-  const handleKeyDownNotesContent = (e) => {
-    if (
-      (e.key === "Enter" && notesContentFocus.focus) ||
-      (e.type === "click" && notesContentFocus.focus)
-    ) {
-      if (notesContentFocus.focus) {
-        const focus = { focus: false, index: nextIndexNote.current };
-        setNotesContentFocus(focus);
-      }
-    } else if (e.type === "click" && !notesContentFocus.focus) {
-      //When user clicks new todo when focus is on notes widget
-      if (notesTitleFocus) {
-        setNotes(
-          produce(notes, (draft) => {
-            draft[selectedIndex].title =
-              draft[selectedIndex].title === ""
-                ? "New Note"
-                : draft[selectedIndex].title;
-            draft[selectedIndex].titleToggle = true;
-          })
-        );
-        setNotesTitleFocus(false);
-      }
-      const newNote = {
-        title: "",
-        id: nextId(),
-        content: "",
-        titleToggle: false,
-        contentToggle: false,
-      };
-      setNotes(
-        produce(notes, (draft) => {
-          draft.push(newNote);
-        })
-      );
-      if (displayedNotes.length === 0) {
-        const focus = { focus: true, index: 0 };
-        setNotesContentFocus(focus);
-      } else {
-        const focus = { focus: true, index: nextIndexNote.current };
-        setNotesContentFocus(focus);
-      }
-    }
-    console.log("handleKeyDown Notes Content function");
-  };
-
+  //Handle Add Notes
   const onClickAddNotes = () => {
-    if (notesTitleFocus) {
-      const newFocus = { focus: false, index: -1 };
-      setNotesContentFocus(newFocus);
-      return;
-    }
     const newNote = {
       title: "",
       id: nextId(),
@@ -277,36 +150,35 @@ const NotesWindow = forwardRef((props, ref) => {
     setSelectedId(newNote.id);
     setSelectedIndex(nextIndexNote.current);
     setNotes(notes.concat(newNote));
-    setNotesTitleFocus(true);
-    const focus = { focus: false, index: -1 };
-    setNotesContentFocus(focus);
+
     nextIndexNote.current += 1;
   };
 
-  const handleContextMenuDeleteNotes = useCallback(() => {
-    setMousePos(initialMousPos);
-
-    setNotes(notes.filter((note) => note.id !== notesIdForContextMenu));
-
-    if (notes.length != 1) {
-      //Reset to first todolist
-      if (notesIdForContextMenu == notes[0].id) {
-        setSelectedId(notes[1].id);
-      } else {
-        setSelectedId(notes[0].id);
-      }
-    } else {
-      setDisplayedNotes(null);
-      setSelectedId(-1);
+  const onEnterNotesList = (e) => {
+    e.preventDefault();
+    closeNoteTextfield();
+  };
+  const handleNotesListClickAway = () => {
+    closeNoteTextfield();
+  };
+  const closeNoteTextfield = () => {
+    if (notes.length == 0) {
+      return;
     }
-    setNotesIdForContextMenu(null);
-    nextIndexNote.current -= 1;
-  });
-  const onNotesContentClick = (e) => {
-    if (notesTitleFocus) {
-      setNotesTitleFocus(false);
-    }
-    setNotesContentFocus(true);
+    const titleTextfieldIndex = notes.findIndex((note) => !note.titleToggle);
+    setNotes(
+      produce(notes, (draft) => {
+        console.log(draft);
+        draft[titleTextfieldIndex].title =
+          draft[titleTextfieldIndex].title === ""
+            ? "New Note"
+            : draft[titleTextfieldIndex].title;
+        draft[titleTextfieldIndex].titleToggle = true;
+      })
+    );
+
+    console.log("handleKeyDown Notes Title function");
+    myRef.current.focus();
   };
 
   const outerstyles = {
@@ -336,92 +208,9 @@ const NotesWindow = forwardRef((props, ref) => {
       handleClickOpen();
     },
   }));
-
-  //When user "DROP" a element, it callbacks this function
-  //So I gotta setNotes index using "array-move"
-  const onSortEnd = ({ oldIndex, newIndex }, e) => {
-    setNotes(arrayMove(notes, oldIndex, newIndex));
+  const handleFocus = (e) => {
+    myRef.current.focus();
   };
-  //value = todo in previous context
-  //I'm basically copying all the code of the "each" notelist as SortableElement
-  //To have draggable function
-  // |-------SortableContainer------|
-  // | |------SortableElement-----| |
-  // | |                          | |
-  // | |                          | |
-  // | |                          | |
-  // | |                          | |
-  // | |                          | |
-  // | |--------------------------| |
-  // |------------------------------|
-
-  const SortableNotelistItem = SortableElement(({ value }) => (
-    <>
-      {value.titleToggle ? (
-        <>
-          <ListItem
-            button
-            selected={selectedId === value.id}
-            onClick={(e) => handleSelectList(e, value.id)}
-            onDoubleClick={handleDoubleClickTitle}
-            onContextMenu={(e) => handleContextMenu(e, value.id)}
-            style={{ zIndex: 9999 }}
-          >
-            <ListItemText primary={value.title}></ListItemText>
-          </ListItem>
-          <Menu
-            keepMounted
-            open={mousePos.mouseY !== null}
-            onClose={handleContextMenuClose}
-            anchorReference="anchorPosition"
-            anchorPosition={
-              mousePos.mouseY !== null && mousePos.mouseX !== null
-                ? { top: mousePos.mouseY, left: mousePos.mouseX }
-                : undefined
-            }
-          >
-            <MenuItem
-              onClick={handleContextMenuDeleteNotes}
-              style={{ color: "#EB5757" }}
-            >
-              <XCircle /> &nbsp; Delete
-            </MenuItem>
-          </Menu>
-          <Divider light />
-        </>
-      ) : (
-        <>
-          <ListItem>
-            <TextField
-              value={value.title}
-              onChange={handleTitleChange}
-              onKeyDown={handleKeyDownNotesTitle}
-              autoFocus
-            ></TextField>
-          </ListItem>
-          <Divider light />
-        </>
-      )}
-    </>
-  ));
-  const SortableNotelist = SortableContainer(({ items }) => (
-    <Grid
-      item
-      xs={4}
-      onClick={handleKeyDownNotesTitle}
-      onContextMenu={handleContextMenu}
-    >
-      <List component="nav" aria-label="to-do lists">
-        {items.map((value, index) => (
-          <SortableNotelistItem
-            key={`note-list-${index}`}
-            value={value}
-            index={index}
-          />
-        ))}
-      </List>
-    </Grid>
-  ));
 
   return (
     <Dialog
@@ -453,20 +242,71 @@ const NotesWindow = forwardRef((props, ref) => {
           spacing={3}
           className="widget-window-content"
         >
-          <SortableNotelist
-            items={notes}
-            axis="y"
-            distance={5}
-            onSortEnd={onSortEnd}
-          />
+          <Grid item xs={4} onContextMenu={handleContextMenu}>
+            <List component="nav" aria-label="to-do lists">
+              {notes.map((note) => (
+                <>
+                  {note.titleToggle ? (
+                    <>
+                      <ListItem
+                        button
+                        selected={selectedId === note.id}
+                        onClick={(e) => handleSelectList(e, note.id)}
+                        onDoubleClick={handleDoubleClickTitle}
+                        onContextMenu={(e) => handleContextMenu(e, note.id)}
+                      >
+                        <ListItemText primary={note.title}></ListItemText>
+                      </ListItem>
+                      <Menu
+                        keepMounted
+                        open={mousePos.mouseY !== null}
+                        onClose={handleContextMenuClose}
+                        anchorReference="anchorPosition"
+                        anchorPosition={
+                          mousePos.mouseY !== null && mousePos.mouseX !== null
+                            ? { top: mousePos.mouseY, left: mousePos.mouseX }
+                            : undefined
+                        }
+                      >
+                        <MenuItem
+                          onClick={handleContextMenuDeleteNotes}
+                          style={{ color: "#EB5757" }}
+                        >
+                          <XCircle /> &nbsp; Delete
+                        </MenuItem>
+                      </Menu>
+                      <Divider light />
+                    </>
+                  ) : (
+                    <>
+                      <ListItem>
+                        <form onSubmit={onEnterNotesList}>
+                          <ClickAwayListener
+                            onClickAway={handleNotesListClickAway}
+                          >
+                            <TextField
+                              value={note.title}
+                              onChange={handleTitleChange}
+                              autoFocus
+                            />
+                          </ClickAwayListener>
+                        </form>
+                      </ListItem>
+                      <Divider light />
+                    </>
+                  )}
+                </>
+              ))}
+            </List>
+          </Grid>
 
           <Divider orientation="vertical" flexItem={true} />
 
-          <Grid item xs={7} md container spacing={3}>
+          <Grid item xs={8} md container spacing={3} onClick={handleFocus}>
             <Grid item container direction="column" spacing={2}>
               <Grid item>
                 <h5>
-                  <b>{displayedNotes == null ? "" : displayedNotes[0].title}</b>
+                  <b>{selectedId == -1 ? "" : notes[selectedIndex].title}</b>
                 </h5>
                 <br></br>
                 <div className="notes-text-style-bar">
@@ -487,34 +327,24 @@ const NotesWindow = forwardRef((props, ref) => {
                 <br></br>
                 <Divider />
                 <Grid item>
-                  {displayedNotes != null ? (
-                    displayedNotes.map((note) => (
-                      <>
-                        <div
-                          style={outerstyles}
-                          onKeyDown={handleKeyDownNotesContent}
-                        >
-                          <TextField
-                            label=""
-                            fullWidth
-                            multiline
-                            InputProps={{ disableUnderline: true }}
-                            //rowsMax={100}
-                            style={innerstyle}
-                            value={note.content}
-                            //onClick={onNotesContentClick}
-                            onChange={handleChangeContent}
-                          />
-                        </div>
-                      </>
-                    ))
+                  {selectedId != -1 ? (
+                    <>
+                      <div style={outerstyles}>
+                        <TextField
+                          label=""
+                          fullWidth
+                          multiline
+                          InputProps={{ disableUnderline: true }}
+                          //rowsMax={100}
+                          style={innerstyle}
+                          value={notes[selectedIndex].content}
+                          onChange={handleChangeContent}
+                          inputRef={myRef}
+                        />
+                      </div>
+                    </>
                   ) : (
-                    <div
-                      style={{ width: "80%" }}
-                      onChange={(e) => handleChangeContent(e)}
-                      onKeyDown={handleKeyDownNotesContent}
-                      autoFocus
-                    ></div>
+                    <></>
                   )}
                 </Grid>
               </Grid>
