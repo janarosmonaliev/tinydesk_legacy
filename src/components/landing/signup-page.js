32db6c6a4bb10c83b3e0415bd06d5242327dc77e
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2021-05-06 14:48:49 +0900

    Update AutoComplete on Register Page

--- src/components/landing/signup-page.js
+++ src/components/landing/signup-page.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 import { navigate } from "gatsby";
 import {
   Grid,
@@ -9,7 +9,9 @@ import {
   MenuItem,
 } from "@material-ui/core";
 import Logo from "../../images/commandt-logo-sm.svg";
-import axios from 'axios';
+import axios from "axios";
+import { Autocomplete } from "@material-ui/lab";
+import cities from "../../cities";
 
 const SignupPage = () => {
   const fullNameRef = useRef();
@@ -20,12 +22,13 @@ const SignupPage = () => {
 
   // Again, using states cuz I don't really understand how Ref works well
 
-  const[fullName, setFullName] = useState("");
-  const[username, setUsername] = useState("");
-  const[email, setEmail] = useState("");
-  const[password, setPassword] = useState("");
-  const[city, setCity] = useState("");
-
+  const [fullName, setFullName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [country, setCountry] = useState("");
+  const [city, setCity] = useState({});
+  const [value, setValue] = useState("");
 
   // register function fires when the user clicks submit
   const register = () => {
@@ -42,7 +45,9 @@ const SignupPage = () => {
       url: "http://localhost:4000/signup",
     }).then((res) => console.log(res));
   };
-
+  const handleOnChangeCountry = (e) => {
+    setCountry(e.target.value);
+  };
   return (
     <Grid item xs={12} md={6} lg={6}>
       <Grid container justify="center">
@@ -63,24 +68,24 @@ const SignupPage = () => {
                   fullWidth
                   label="Full name"
                   type="name"
-                  inputRef={fullNameRef} 
-                  onChange={e => setFullName(e.target.value)}
+                  inputRef={fullNameRef}
+                  onChange={(e) => setFullName(e.target.value)}
                 />
                 <TextField
                   id="sign-page-form-username"
                   fullWidth
                   label="Username"
                   type="text"
-                  inputRef={usernameRef} 
-                  onChange={e => setUsername(e.target.value)}
+                  inputRef={usernameRef}
+                  onChange={(e) => setUsername(e.target.value)}
                 />
                 <TextField
                   id="sign-page-form-email"
                   fullWidth
                   label="Email"
                   type="email"
-                  inputRef={emailRef} 
-                  onChange={e => setEmail(e.target.value)}
+                  inputRef={emailRef}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
                 <TextField
                   id="sign-page-form-password"
@@ -88,24 +93,36 @@ const SignupPage = () => {
                   label="Password"
                   type="password"
                   inputRef={passwordRef}
-                  autoComplete="current-password" 
-                  onChange={e => setPassword(e.target.value)}
+                  autoComplete="current-password"
+                  onChange={(e) => setPassword(e.target.value)}
                 />
+
                 <TextField
                   id="sign-page-form-city"
                   select
                   fullWidth
-                  label="Current city"
-                  inputRef={cityRef} 
-                  onChange={e => setCity(e.target.value)}
+                  label="Country"
+                  onChange={(e) => handleOnChangeCountry(e)}
                 >
-                  <MenuItem key="Seoul" value="Seoul">
-                    Seoul
+                  <MenuItem key="usa" value="usa">
+                    United State
                   </MenuItem>
-                  <MenuItem key="Incheon" value="Incheon">
-                    Incheon
+                  <MenuItem key="korea" value="korea">
+                    South Korea
                   </MenuItem>
                 </TextField>
+                <Autocomplete
+                  onChange={(e, newValue) => {
+                    setCity(newValue);
+                  }}
+                  id="city-by-country"
+                  options={cities[country]}
+                  getOptionLabel={(option) => option.name}
+                  disabled={cities[country] == null}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Cities" variant="standard" />
+                  )}
+                ></Autocomplete>
               </form>
 
               <Button
