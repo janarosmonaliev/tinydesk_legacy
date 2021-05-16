6a5f84451c9d427da3a4fa7aeca4755b497f6473
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2021-05-16 17:24:14 +0900

    Revert "Update client.js"
    This reverts commit 47c3cca5b8ed66dfb12c41ffc0f23639a0d6b0f4.

--- src/api/client.js
+++ src/api/client.js
@@ -2,14 +2,9 @@ import axios from "axios";
 
 const client = axios.create({
   withCredentials: true,
-  baseURL: "https://test-project-416.herokuapp.com",
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-  },
 });
 
-// client.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
-// client.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
-// client.defaults.headers.post["Access-Control-Allow-Methods"]
+client.defaults.baseURL = "https://test-project-416.herokuapp.com";
+client.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
+client.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 export default client;
