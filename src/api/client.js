0e7adfc40a0d6dbe8503e80260bba84316ef828f
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2021-05-16 13:15:33 +0900

    Test URL

--- src/api/client.js
+++ src/api/client.js
@@ -4,6 +4,6 @@ const client = axios.create({
   withCredentials: true,
 });
 
-client.defaults.baseURL = "https://commandt-backend.herokuapp.com";
+client.defaults.baseURL = "https://test-project-416.herokuapp.com";
 
 export default client;
