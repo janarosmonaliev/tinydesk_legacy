47c3cca5b8ed66dfb12c41ffc0f23639a0d6b0f4
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2021-05-16 16:56:31 +0900

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -2,9 +2,14 @@ import axios from "axios";
 
 const client = axios.create({
   withCredentials: true,
+  baseURL: "https://test-project-416.herokuapp.com",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
 });
 
-client.defaults.baseURL = "https://test-project-416.herokuapp.com";
-client.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
-client.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+// client.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
+// client.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+// client.defaults.headers.post["Access-Control-Allow-Methods"]
 export default client;
