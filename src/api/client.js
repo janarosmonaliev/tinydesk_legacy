f8a1586856676118d30e11a3d389c62591fa19a3
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2021-05-16 15:53:20 +0900

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -5,5 +5,6 @@ const client = axios.create({
 });
 
 client.defaults.baseURL = "https://test-project-416.herokuapp.com";
-
+client.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
+client.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 export default client;
