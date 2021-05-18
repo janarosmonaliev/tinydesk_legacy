1b4dd7142dea2171d59e9f19a4925a0d069b6d89
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2021-05-18 22:30:25 +0900

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -4,9 +4,7 @@ const client = axios.create({
   withCredentials: true,
 });
 
-client.defaults.baseURL = "https://test-project-416.herokuapp.com";
+client.defaults.baseURL = "https://commandt-backend.herokuapp.com/";
 // client.defaults.baseURL = "http://localhost:4000";
-// client.defaults.headers.post["Content-Type"] = "text/html; charset=utf-8";
-// client.defaults.headers.common["Authorization"] = "Bearer a1b2c3d4";
-// client.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+
 export default client;
