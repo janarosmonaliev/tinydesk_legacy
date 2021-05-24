1b57ed5c6510252a83d218034d1eaf6bfc41cd46
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2021-05-24 13:58:57 +0900

    Update fetch.js

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -5,31 +5,30 @@ export const getUserData = (setter) => {
   client.get("/home").then((res) => {
     if (res.data == "no uid") {
       navigate("/login");
-      return;
-    }
-    console.log("HELLO");
-    setter.setFolders(res.data.folders);
-    setter.setBackground(res.data.backgroundImg.url);
-    setter.setUnicorn(res.data.keepUnicorn);
-    setter.setEmail(res.data.email);
-    setter.setUsername(res.data.username);
-    setter.setLocation(res.data.location);
-    setter.setDisplayedBookmarks(res.data.folders[0].bookmarks);
-    setter.setSelectedFolderId(res.data.folders[0]._id);
-    if (res.data.todolists.length != 0) {
-      res.data.todolists.forEach((tl) => {
-        tl["toggle"] = true;
-        tl["isUpdated"] = false;
-        tl.todos.forEach((todo) => {
-          todo["toggle"] = true;
-          todo["isUpdated"] = false;
+    } else {
+      setter.setFolders(res.data.folders);
+      setter.setBackground(res.data.backgroundImg.url);
+      setter.setUnicorn(res.data.keepUnicorn);
+      setter.setEmail(res.data.email);
+      setter.setUsername(res.data.username);
+      setter.setLocation(res.data.location);
+      setter.setDisplayedBookmarks(res.data.folders[0].bookmarks);
+      setter.setSelectedFolderId(res.data.folders[0]._id);
+      if (res.data.todolists.length != 0) {
+        res.data.todolists.forEach((tl) => {
+          tl["toggle"] = true;
+          tl["isUpdated"] = false;
+          tl.todos.forEach((todo) => {
+            todo["toggle"] = true;
+            todo["isUpdated"] = false;
+          });
         });
-      });
-      setter.setTodolists(res.data.todolists);
-    }
-    res.data.notes.forEach((note) => (note["toggle"] = true));
-    setter.setNotes(res.data.notes);
+        setter.setTodolists(res.data.todolists);
+      }
+      res.data.notes.forEach((note) => (note["toggle"] = true));
+      setter.setNotes(res.data.notes);
 
-    setter.setLoading(false);
+      setter.setLoading(false);
+    }
   });
 };
