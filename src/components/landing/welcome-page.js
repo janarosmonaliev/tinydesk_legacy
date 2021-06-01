e1b19d5129be29252f0e32d03a2bd4bc24db00b0
Fabio Calero <fabio.calero@stonybrook.edu>
2021-06-01 20:46:52 +0900

    sorry about the previous commit, it wasn't production ready

--- src/components/landing/welcome-page.js
+++ src/components/landing/welcome-page.js
@@ -15,7 +15,7 @@ const useStyles = makeStyles({
 });
 
 const loginWithGoogle = () => {
-  window.open("http://localhost:4000/google", "_self");
+  window.open("https://commandt-backend.herokuapp.com/google", "_self");
 }
 
 const WelcomePage = () => {
